1e668bb67c72591b2adb873e4e08bddf3a6f12f7
rajrupdasofficial <rajrupofficialid@protonmail.com>
2024-08-25 17:36:08 +0530

    fixed image uploading logic
    saving image metadata into database - deno kv
    
    return the response from the appwrite
    
    some general improvements added
    
    Signed-off-by: rajrupdasofficial <rajrupofficialid@protonmail.com>

--- config/convexconfig/appwriteconfig.ts
+++ config/convexconfig/appwriteconfig.ts
@@ -1,5 +1,11 @@
 import { Client, Storage } from "npm:appwrite";
 import "@std/dotenv/load";
+
+interface ImageMetadata {
+  filename: string;
+  useruid: string;
+}
+
 const appwritecloud = Deno.env.get("APPWRITE_URI");
 const appwriteprojectid = Deno.env.get("APPWRITE_PROJECT_ID");
 const appwritestorageid = Deno.env.get("APPWRITE_STORAGE_BUCKET_ID");
@@ -9,20 +15,32 @@ const client = new Client()
 
 const storage = new Storage(client);
 
-const uploadappwrite = (file: any) => {
-  const promise = storage.createFile(
-    `${appwritestorageid}`,
-    crypto.randomUUID(),
-    file,
-  );
-  promise.then(
-    function (response: any) {
-      console.log("Upload successful:", response); // Success
-    },
-    function (error: any) {
-      console.error("Upload failed:", error); // Failure
-    },
-  );
+const uploadappwrite = async (
+  file: any,
+  user_uid: any,
+): Promise<{ success?: any; error?: string }> => {
+  try {
+    // Create a file in Appwrite storage
+    const randomimageid = crypto.randomUUID();
+    const response = await storage.createFile(
+      `${appwritestorageid}`,
+      randomimageid,
+      file,
+    );
+    //save image metadata in kv
+    console.log("randomuseridinappwriteconfig", user_uid);
+    const kv = await Deno.openKv();
+    const imagemetadata: ImageMetadata = {
+      filename: file.name,
+      useruid: user_uid,
+    };
+    await kv.set(["imagemetadata", randomimageid], imagemetadata);
+    console.log("Upload successful:", response); // Success log
+    return { success: response }; // Return successful response
+  } catch (error) {
+    console.error("Upload failed:", error); // Error log
+    return { error: error.message || "An error occurred during upload." }; // Return error response
+  }
 };
 
 export default uploadappwrite;
